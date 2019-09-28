6bb9e1fd465299b437100b61327514b851ca8d69
leonbagdag <dsrt1991@gmail.com>
2019-09-28 12:28:33 +0000

    completo

---
+++ src/js/component/Card.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+//create your first component
+export const Card = () => {
+	return (
+		<div className="container">
+			<div class="card-deck">
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
